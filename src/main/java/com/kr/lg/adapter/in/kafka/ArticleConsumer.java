7363b1bc9264f12130c390d2d6b55c8223d1996d
beomshin.kim <chzh859@naver.com>
2024-11-04 22:06:50 +0900

    # kafka + 헥사고날 아키텍쳐 - 로우지지 게시판, 트라이얼 등록

--- src/main/java/com/kr/lg/adapter/in/kafka/ArticleConsumer.java
+++ src/main/java/com/kr/lg/adapter/in/kafka/ArticleConsumer.java
@@ -17,12 +17,23 @@ public class ArticleConsumer {
 
     private final ArticleInsertUseCase articleInsertUseCase;
 
-    @KafkaListener(topics = "${kafka.topic.alarm.name}", groupId = "${kafka.consumer.group-id}", containerFactory = "containerFactory")
+    @KafkaListener(topics = "${kafka.topic.article.name}", groupId = "${kafka.consumer.group-id}", containerFactory = "containerFactory")
     public void enroll(String msg, Acknowledgment ack) { // offset 커밋 조절
         try {
             log.info("◆ 게시판 등록 카프카 메세지 consumer 시작");
             articleInsertUseCase.enroll(ArticleInsertCommand.of(new ObjectMapper().readValue(msg, ArticleMsg.class)));
             log.info("◆ 게시판 등록 카프카 메세지 consumer 종료");
+        } catch (Exception e) {
+            log.error("", e);
+        }
+        ack.acknowledge();
+    }
+
+
+    @KafkaListener(topics = "${kafka.topic.comment.name}", groupId = "${kafka.consumer.group-id}", containerFactory = "containerFactory")
+    public void comment(String msg, Acknowledgment ack) { // offset 커밋 조절
+        try {
+
         } catch (Exception e) {
             log.error("", e);
         }
